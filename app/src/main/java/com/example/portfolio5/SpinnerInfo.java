8c1d7d29052decb657afca948127511df5b6ccb1
justin-dupre <justinnedits@gmail.com>
2019-02-27 10:38:49 -0600

    first commit

---
+++ app/src/main/java/com/example/portfolio5/SpinnerInfo.java
@@ -0,0 +1,9 @@
+package com.example.portfolio5;
+
+
+public class SpinnerInfo {
+    static public String valueArray[] = {"Chocolate Chip", "Sugar", "Peanut Butter", "Macaron"};
+
+
+
+}//end spinnerinfo class
